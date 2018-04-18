73c8690ecaaea0bf3b398b2e31540bec51095d97
pimOstendorf <pimwing@gmail.com>
2018-04-18 17:34:37 +0200

    some controlls added lmao

---
+++ Assets/Scripts/SwordControll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordControll : MonoBehaviour {
+
+    private Vector3 savedPos;
+    public float multiplier = 5;
+
+	// Use this for initialization
+	void Start () {
+        savedPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = savedPos + new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * multiplier;
+	}
+}
