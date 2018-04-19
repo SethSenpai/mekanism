7d2e01938756d8474848d2a23f1e2e7975366bdf
pimOstendorf <pimwing@gmail.com>
2018-04-19 16:35:59 +0200

    he punch

---
+++ Assets/Scripts/HandControll.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandControll : MonoBehaviour {
+
+    private Vector3 savedPos;
+    public bool leftHand = false;
+    public float multiplier = 5;
+    public float maxDistance = 1;
+
+    // Use this for initialization
+    void Start () {
+        savedPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (leftHand)
+        {
+
+            Vector2 contPos = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            Vector2 outputPos = GetConstrainedPosition(new Vector2(0, 0),contPos);
+            Vector3 clampedPos = new Vector3(outputPos.x, outputPos.y, 0);
+            this.transform.position = savedPos + clampedPos; 
+        }
+        else
+        {
+            Vector2 contPos = new Vector2(Input.GetAxis("HorizontalRight"), Input.GetAxis("VerticalRight"));
+            Vector2 outputPos = GetConstrainedPosition(new Vector2(0, 0), contPos);
+            Vector3 clampedPos = new Vector3(outputPos.x, outputPos.y, 0);
+            this.transform.position = savedPos + clampedPos;
+        }
+	}
+
+    Vector2 GetConstrainedPosition(Vector2 midPoint, Vector2 endPoint)
+    {
+        //get the length of the line
+        float dist = Vector2.Distance(midPoint, endPoint);
+
+        //Check for max length
+        if (Vector2.Distance(midPoint, endPoint) > maxDistance)
+        {
+            //get the direction of the line between mid point and end point
+            Vector2 dirVector = endPoint - midPoint;
+
+            //normalize
+            dirVector.Normalize();
+            //Debug.Log((dirVector * maxDistance) + midPoint);
+            //return the clamped position
+            return (dirVector * maxDistance) + midPoint;
+            
+        }
+        //return the original position
+        return endPoint;
+    }
+}
---
+++ Assets/Scripts/sceneControll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class sceneControll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp("1"))
+        {
+            Debug.Log("loading Fist Test");
+            SceneManager.LoadScene("fistTest");
+        }
+        if (Input.GetKeyUp("2"))
+        {
+            Debug.Log("loading Sword Test");
+            SceneManager.LoadScene("swordTest");
+        }
+    }
+}
